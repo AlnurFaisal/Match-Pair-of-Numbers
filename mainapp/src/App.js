b798287b05ab86f981f6e95006a1d185bddddd30
Alnur_Faisal <alnurfaisal@outlook.com>
2018-03-28 10:59:34 +0800

    refactor code to bring data to game component

--- mainapp/src/App.js
+++ mainapp/src/App.js
@@ -2,24 +2,12 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Game from "./Game/Game";
-import { doubleArray, shuffle, generatePositiveNumber } from "./utils/utils";
 
 class App extends Component {
   render() {
-    return <Game gameMap={this.genRandomNumbers()} />;
+    return <Game/>;
   }
 
-  genRandomNumbers() {
-    let arr = [];
-    const ITERATIONS = 2;
-    for (let i = 0; i < ITERATIONS; i++) {
-      arr.push(generatePositiveNumber());
-    }
-    const doubleArr = doubleArray(arr);
-    const shuffleArray = shuffle(doubleArr);
-    //const gameObj = {}
-    return shuffleArray;
-  }
 }
 
 export default App;
--- mainapp/src/Game/Game.js
+++ mainapp/src/Game/Game.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Square from "../Square/Square";
+import { doubleArray, shuffle, generatePositiveNumber } from "../utils/utils";
 import "./Game.css";
 
 class Game extends Component {
   constructor() {
     super();
+    this.state ={
+      gameMap:this.genRandomNumbers()
+    }
   }
 
   render() {
@@ -13,7 +17,7 @@ class Game extends Component {
         <div class="row">
           <div class="col-md-7 offset-md-1">
             <div className="css-grid-container1">
-              {this.props.gameMap.map((element, i) => {
+              {this.state.gameMap.map((element, i) => {
                 return <Square key={i} number={element} />;
               })}
             </div>
@@ -21,6 +25,18 @@ class Game extends Component {
         </div>
       </div>
     );
+  }
+
+  genRandomNumbers() {
+    let arr = [];
+    const ITERATIONS = 2;
+    for (let i = 0; i < ITERATIONS; i++) {
+      arr.push(generatePositiveNumber());
+    }
+    const doubleArr = doubleArray(arr);
+    const shuffleArray = shuffle(doubleArr);
+    //const gameObj = {}
+    return shuffleArray;
   }
 }
 
