d9d9218509e54be88a6fb9aebf416ffb990452cb
davidtan <davidtan@thoughtworks.com>
2018-04-05 17:32:38 +0800

    [David] Add code review comments

--- src/Home/HomeNav.js
+++ src/Home/HomeNav.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./Home";
 import Home2 from "./Home2";
 
 const HomeNav = () => (
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/game" component={Home2} />  
-    </div>
+  <div>
+    {/* I think Home could be better named to WelcomePage and Home2 could be better named as GameController */}
+    <Route exact path="/" component={Home} />
+    <Route exact path="/game" component={Home2} />
+  </div>
 );
 
 export default HomeNav;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,18 @@
+export const genRandomNumbers = gameLevel => {
+  let arr = [];
+  const ITERATIONS = gameLevel;
+  for (let i = 0; i < ITERATIONS; i++) {
+    arr.push(generatePositiveNumber());
+  }
+  const doubleArr = doubleArray(arr);
+  const shuffleArray = shuffle(doubleArr);
+  const gameObj = [];
+  for (let i = 0; i < shuffleArray.length; i++) {
+    gameObj[i] = { value: shuffleArray[i], isMatched: false, show: false };
+  }
+  return gameObj;
+};
+
 export const doubleArray = arr => {
   return [...arr, ...arr];
 };
@@ -25,4 +40,3 @@ export const shuffle = array => {
 export const generatePositiveNumber = () => {
   return Math.floor(Math.random() * 100 + 1);
 };
-
