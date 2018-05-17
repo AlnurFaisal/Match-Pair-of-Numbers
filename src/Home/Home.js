f4f6ed6a561bf478747e95db61e93598258b8895
Alnur_Faisal <alnurfaisal@outlook.com>
2018-05-17 19:57:51 +0800

    Added leaderboard component that renders the scores of all the players that played the game

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,20 +2,37 @@ import React, { Component } from "react";
 import { Button } from "reactstrap";
 import "./Home.css";
 import Player from "../Player/Player";
+import Leaderboard from "../Leaderboard/Leaderboard";
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-      redirect: false
+      redirect: false,
+      leaderboard: false
     };
 
     this.runRedirect = this.runRedirect.bind(this);
+    this.leaderboard = this.leaderboard.bind(this);
+    this.compare = this.compare.bind(this);
   }
 
   render() {
-    if(this.state.redirect) {
-      return <Player players={this.props.players} updatePlayers={this.props.updatePlayers} />;
+    if (this.state.redirect) {
+      return (
+        <Player
+          players={this.props.players}
+          updatePlayers={this.props.updatePlayers}
+        />
+      );
+    } else if (this.state.leaderboard) {
+      return (
+        <Leaderboard
+          sortPlayers={this.sortScore.bind(this)}
+          players={this.props.players}
+          updatePlayers={this.props.updatePlayers}
+        />
+      );
     }
     return (
       <div>
@@ -27,8 +44,23 @@ class Home extends Component {
                 <p className="hometext">Click on the button below to begin!</p>
               </div>
               <div className="offset-md-3 col-md-6 col-xs-12">
-                <Button size="lg" color="success" onClick={this.runRedirect} className="shadow" block>
+                <Button
+                  size="lg"
+                  color="success"
+                  onClick={this.runRedirect}
+                  className="shadow"
+                  block
+                >
                   Begin
+                </Button>
+                <Button
+                  size="lg"
+                  color="secondary"
+                  onClick={this.leaderboard}
+                  className="shadow"
+                  block
+                >
+                  Leaderboard
                 </Button>
               </div>
             </div>
@@ -42,6 +74,31 @@ class Home extends Component {
     this.setState({
       redirect: true
     });
+  }
+
+  leaderboard() {
+    this.setState({
+      leaderboard: true
+    });
+  }
+
+  sortScore() {
+    let copyPlayers = [...this.props.players];
+    copyPlayers.sort(this.compare);
+    return copyPlayers;
+  }
+
+  compare(a, b) {
+    const scoreA = a.score;
+    const scoreB = b.score;
+
+    let comparison = 0;
+    if (scoreA > scoreB) {
+      comparison = -1;
+    } else if (scoreA < scoreB) {
+      comparison = 1;
+    }
+    return comparison;
   }
 }
 
---
+++ src/Leaderboard/Leaderboard.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { Card, Button } from "reactstrap";
+import Home from "../Home/Home";
+import "./Leaderboard.css";
+
+class Leaderboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: props.sortPlayers(),
+      home: false
+    };
+
+    this.goBack = this.goBack.bind(this);
+  }
+
+  render() {
+    if (this.state.home) {
+      return (
+        <Home
+          players={this.props.players}
+          updatePlayers={this.props.updatePlayers}
+        />
+      );
+    }
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 col-xs-12">
+            <h1 className="headline" align="center">
+              Leaderboard
+            </h1>
+            <Card body>
+              <div className="table-responsive">
+                <table className="table">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Level</th>
+                      <th scope="col">Score</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.players.map((element, i) => {
+                      return (
+                        <tr key={i}>
+                          <th scope="row">{i + 1}</th>
+                          <td>{element.name}</td>
+                          <td>{element.level}</td>
+                          <td>{element.score}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </Card>
+            <br />
+            <div className="offset-md-3 col-md-6 col-xs-12">
+              <Button
+                size="lg"
+                color="success"
+                onClick={this.goBack}
+                className="btn shadow"
+                block
+              >
+                Go Back
+              </Button>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  goBack() {
+    this.setState({
+      home: true
+    });
+  }
+}
+
+export default Leaderboard;
